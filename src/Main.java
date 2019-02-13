59160247234aa1a04c225a8a3237154cb511dd86
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-02-13 11:33:11 +0200

    add variables

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        /*Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();*/
         //Console.ReadLine();
+
+        int a = 1;
+        int b = 2;
     }
 }
 //var a:integer;
