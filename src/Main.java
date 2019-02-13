c2a64643ce23380847db462613f7998e83504ccb
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-02-13 11:14:36 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        //Console.ReadLine();
+    }
+}
+//var a:integer;
+//begin
+//writeln('Hello world!');
+//read(a);
+//end.
